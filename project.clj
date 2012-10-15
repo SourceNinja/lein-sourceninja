5dc0741d639d244bdface5d88715517bc68257fc
Matt Stump <mstump@sourceninja.com>
2012-10-15 10:57:20 -0700

    added the sourceninja lein plugin implementation

---
+++ project.clj
@@ -0,0 +1,16 @@
+(defproject lein-sourceninja "0.1.0"
+  :description "A Leiningen plugin for SourceNinja."
+  :url "https://github.com/SourceNinja/lein-sourceninja"
+  :license {:name "AGPL"
+            :url "http://www.gnu.org/licenses/agpl-3.0.html"}
+
+  :dependencies [[cheshire "4.0.3"]
+                 [clj-http "0.5.5"]
+                 [com.cemerick/pomegranate "0.0.13"]
+                 [leinjacker "0.3.0"]
+                 [org.clojure/clojure "1.4.0"]]
+
+  :sourceninja {:id "973101d5-283d-4dda-af16-2ceff25af419"
+                :token "9886f3dbbeb7b70d53aee8039cd50748"}
+
+  :eval-in-leiningen true)
